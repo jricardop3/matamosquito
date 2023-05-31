18da6d30864d6ef0bde05ca92481cf737bcd75bb
Jose Ricardo Peixoto <peixoto.service@hotmail.com>
2023-05-30 21:04:53 -0300

    parte 2
    posicionando mosquito

--- js/jogo.js
+++ js/jogo.js
@@ -3,7 +3,23 @@ let largura = 0
 function ajustaJogo (){
     altura = window.innerHeight
     largura = window.innerWidth
-    document.write (largura,  altura)
+    
 }
 ajustaJogo()
 
+function posicaoRandomica(){
+    let posicaoX = Math.floor(Math.random() * largura) -100
+    let posicaoY = Math.floor(Math.random() * altura) -100
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+    document.write (posicaoX,'<br>'+  posicaoY)
+
+    let mosquito = document.createElement('img')
+    mosquito.src = 'img/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
