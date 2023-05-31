b4635129387329fa8e64ad46a59b7f1b47ad56c8
Jose Ricardo Peixoto <peixoto.service@hotmail.com>
2023-05-30 22:50:01 -0300

    parte 4
    background, e controlador de vidas.

--- js/jogo.js
+++ js/jogo.js
@@ -1,5 +1,6 @@
 let altura = 0
 let largura = 0
+let vidas = 1
 function ajustaJogo (){
     altura = window.innerHeight
     largura = window.innerWidth
@@ -8,18 +9,32 @@ function ajustaJogo (){
 ajustaJogo()
 
 function posicaoRandomica(){
+if (document.getElementById('mosquito')) {
+    document.getElementById('mosquito').remove()
+    if (vidas > 3 ) {
+        alert('Game Over!')
+        
+    }else {
+    document.getElementById('v'+ vidas).src="img/coracao_vazio.png"
+    vidas ++
+    }
+}
     let posicaoX = Math.floor(Math.random() * largura) -100
     let posicaoY = Math.floor(Math.random() * altura) -100
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
-    document.write (posicaoX,'<br>'+  posicaoY)
+    
 
     let mosquito = document.createElement('img')
     mosquito.src = 'img/mosca.png'
-    mosquito.className = TamanhoAleatorio()
+    mosquito.className = TamanhoAleatorio() +' '+ ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito' 
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
     TamanhoAleatorio()
@@ -35,4 +50,14 @@ function TamanhoAleatorio (){
             return 'mosquito2'
     }
     
+}
+
+function ladoAleatorio (){
+    let classe = Math.floor(Math.random() * 2)
+    switch(classe){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
 }
